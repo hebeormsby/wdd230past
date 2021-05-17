194d641822c074e9dea0e8129c4d2f1b358ad5de
hebeormsby <hebeormsby@gmail.com>
2021-05-17 09:38:52 -0400

    update

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,8 @@ const year = { year: "numeric" };
 
 const date = document.lastModified;
 
-document.getElementById("year").textContent = new Date().toLocaleDateString(
+document.querySelector("#year").textContent = new Date().toLocaleDateString(
   "en-US",
   year
 );
-document.getElementById("date").textContent = date;
+document.querySelector("#date").textContent = date;
